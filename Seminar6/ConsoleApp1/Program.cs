0d1857b5914b626ff1ee0e109dc8724bd3916eb8
Tatiana Savchuk <redtitania2045@gmail.com>
2022-11-17 11:22:30 +0300

    homework for Seminar 6

---
+++ Seminar6/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿ /*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+
+Console.WriteLine("Склько чисел Вы хотите ввести? : ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[]array = new int[M];
+int sum = 0;
+
+for(int i = 0; i<M; i++)
+{
+    
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    array[i] = number;
+    
+    if (array[i] > 0)
+    {
+        sum++;
+    }
+}
+
+Console.WriteLine($"Выввели {sum} положительных чисел.");
