0ba4196976c7575781b4ce7b1d31067c767ad2fe
Tatiana Savchuk <redtitania2045@gmail.com>
2022-11-28 19:52:37 +0300

    homework seminar 8

---
+++ Seminar8/ConsoleApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int [,] array = new int[4,5];
+int [] sum = new int [5];
+for (int i = 0; i < 5; i++)
+{
+    sum[i] = 0;
+   
+}
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = 0; i < 4; i++)
+{
+    for ( int j = 0; j < 5; j++)
+    {
+        array[i,j] = new Random().Next(1,11);
+        Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+
+for (int i = 0; i < 4; i++)
+{
+    for ( int j = 0; j < 5; j++)
+    {
+        sum[i] = sum[i] + array[i,j];
+        
+    }
+    
+    Console.Write(sum[i] + "  ");       
+}
+            
+Console.WriteLine();
+
+
+int min = 0;
+int minRow = 0;
+
+for (int i = 0; i < 4; i++)
+{
+     min = sum[i];
+
+    if (sum[i] < min)
+    {
+       min = sum[i];
+       minRow = i+1;
+
+    }
+    
+}
+    Console.Write($"minimal sum row  = {minRow}");
+
+     
---
+++ Seminar8/ConsoleApp3/Program.cs
@@ -0,0 +1,53 @@
+﻿/*адайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+int [,]array = new int [2,2];
+int [,]array2 = new int [2,2];
+int [,]result = new int [2,2];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for ( int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(1,11);
+        Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < array2.GetLength(0); i++)
+{
+    for ( int j = 0; j < array2.GetLength(1); j++)
+    {
+        array2[i,j] = new Random().Next(1,11);
+        Console.Write(array2[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+         result[i, j] = 0;
+ 
+                for (int k = 0; k < array.GetLength(1); k++)
+                {
+                    result[i, j] = array[i, k] * array2[k, j];
+                    
+                }
+            
+        Console.Write(result[i,j] + " ");
+    }
+    Console.WriteLine();
+}
+
