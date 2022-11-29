ef8cb66f3f87e2e6a467964f2b8f76fd8103426f
Tatiana Savchuk <redtitania2045@gmail.com>
2022-11-29 17:02:23 +0300

    homework for seminar 9

---
+++ Seminar9/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+int ShowNumbers (int n)
+{
+if (n == 1)
+{
+    return 1;  
+}
+Console.Write (n + " ");
+return ShowNumbers( n - 1);
+}
+
+
+
+
+Console.WriteLine("Enter number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = ShowNumbers(n);
+Console.Write (result);
+
---
+++ Seminar9/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 301"
+
+int MtoNnumbers (int m, int n)
+{
+   Console.Write (m + " ");
+
+   if(m == n)
+   {
+    return n;
+   }
+   return MtoNnumbers (m + 1, n);
+
+}
+
+Console.WriteLine("Enter number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < m)
+
+{
+    int temp = n;
+    n = m;
+    m = temp;
+}
+
+
+MtoNnumbers(m,n);
